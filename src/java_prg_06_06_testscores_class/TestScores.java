97b0ec3a1a81351236feb6545fcc7b720be9ba6d
aeiou <aeiou@10.8.61.85>
2018-01-16 11:56:31 -0500

    TestScores class created.

---
+++ src/java_prg_06_06_testscores_class/TestScores.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_06_06_testscores_class;
+
+/**
+ *
+ * @author aeiou
+ */
+public class TestScores {
+    private float fltScore1;
+    private float fltScore2;
+    private float fltScore3;
+    
+    public TestScores(float score1, float score2, float score 3) {
+        fltScore1 = score1;
+        fltScore2 = score2;
+        fltScore3 = score3;
+    }
+    
+    public TestScores() {
+        fltScore1 = 0.0;
+        fltScore2 = 0.0;
+        fltScore3 = 0.0;
+    }
+    
+    public 
+}
